003d793b192981bcec2f0806dc951da6f3788634
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-11-10 21:47:41 +0000

    added setup options to the start of the script

--- excel_importer.go
+++ excel_importer.go
@@ -11,8 +11,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func connectdb() *sql.DB {
-	connStr := "postgresql://localhost/importedExcelSheets?user=richardgannon&password=postgres&sslmode=disable"
+func connectdb(pgUser string, pgPassword string) *sql.DB {
+	connStr := "postgresql://localhost/importedExcelSheets?user=" + pgUser + "&password=" + pgPassword + "&sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Println(err)
@@ -20,19 +20,31 @@ func connectdb() *sql.DB {
 	return db
 }
 func main() {
+	var documentName string
+	var pgUser string
+	var pgPassword string
+	fmt.Println("Enter your postgres username: ")
+	fmt.Scanln(&pgUser)
+	fmt.Println("Enter your postgres password: ")
+	fmt.Scanln(&pgPassword)
+	fmt.Println("Enter the name of the excel document: ")
+	fmt.Scanln(&documentName)
 	started := time.Now()
 
-	f, err := excelize.OpenFile("EmployeeSampleData.xlsx")
+	f, err := excelize.OpenFile(documentName + ".xlsx")
 	if err != nil {
 		fmt.Println(err)
+		fmt.Printf("Error opening file")
 		return
 	}
 
-	rows := f.GetRows("Data")
+	firstSheet := f.WorkBook.Sheets.Sheet[0].Name
+
+	rows := f.GetRows(firstSheet)
 
 	fmt.Printf("Total rows: %d\n", len(rows))
 
-	db := connectdb()
+	db := connectdb(pgUser, pgPassword)
 
 	ColumnHeadersWithSpaces := rows[0]
 	var columnHeaders []string
@@ -48,13 +60,13 @@ func main() {
 		columnHeaders = append(columnHeaders, column)
 	}
 
-	_, err = db.Exec("CREATE TABLE employees (id SERIAL PRIMARY KEY)") // TODO: change this to a variable
+	_, err = db.Exec("CREATE TABLE " + documentName + " (id SERIAL PRIMARY KEY)")
 	if err != nil {
 		fmt.Println("\nError creating table", err)
 	}
 
 	for _, column := range columnHeaders {
-		_, err := db.Exec("ALTER TABLE employees ADD COLUMN " + column + " VARCHAR(255)") // TODO: change this to a variable
+		_, err := db.Exec("ALTER TABLE " + documentName + " ADD COLUMN " + column + " VARCHAR(255)")
 		if err != nil {
 			fmt.Println("\nError adding column: ", err)
 			fmt.Printf("Column: %s", column)
@@ -64,16 +76,15 @@ func main() {
 	id := 1
 	for _, row := range rows[1:] {
 		columnIndex := 0
-		_, err := db.Exec("INSERT INTO employees (id) VALUES ($1)", id)
+		_, err := db.Exec("INSERT INTO "+documentName+" (id) VALUES ($1)", id)
 		if err != nil {
 			fmt.Println("\nError inserting id: ", err)
 		}
 		for _, cell := range row {
 
 			idforSQL := strconv.Itoa(id)
-			sqlStatement := "UPDATE employees SET " + columnHeaders[columnIndex] + " = '" + cell + "' WHERE id = " + idforSQL
-			// fmt.Println(sqlStatement)
-			_, err := db.Exec(sqlStatement) // TODO: change this to a variable
+			sqlStatement := "UPDATE " + documentName + " SET " + columnHeaders[columnIndex] + " = '" + cell + "' WHERE id = " + idforSQL
+			_, err := db.Exec(sqlStatement)
 			if err != nil {
 				fmt.Println("\nError inserting data: ", err)
 				fmt.Printf("Cell: %s, Column: %s, ID: %d", cell, columnHeaders[columnIndex], id)
