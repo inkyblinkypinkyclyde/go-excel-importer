27563ad1c182829623a41dbdf20bc594d44d51fa
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-11-13 21:14:26 +0000

    removed bug with duplicate null JSONs

--- excel_importer.go
+++ excel_importer.go
@@ -3,9 +3,6 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
-	// "net/http"
-
 	"strconv"
 	"strings"
 	"time"
@@ -15,38 +12,18 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type ExcelData struct {
-	pgUser     string
-	pgPassword string
-	pgTable    string
-	db         *sql.DB
-}
-
-var documentName string = "EmployeeSampleData"
-var pgUser string = "richardgannon"
-var pgPassword string = "postgres"
-
-var dbValues = &ExcelData{pgUser, pgPassword, documentName, connectdb(pgUser, pgPassword)}
-
-func getColumnNames() []string { // gets the column headers from the db
-	f, err := dbValues.db.Query("SELECT * FROM " + dbValues.pgTable + " WHERE 1=0")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-	columnNames, err := f.Columns()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return columnNames
-}
+var pgUser string
+var pgPassword string
+var documentName string
+var columnHeaders []string
+var db *sql.DB
 
 func getData(c *gin.Context) {
 	column := c.Param("column") // this is pulled form the url
-	datum := c.Param("datum")   // this is pulled from the url
+	datum := c.Param("datum")   // this is pulled form the url
 
-	sql_query := "SELECT id FROM " + dbValues.pgTable + " WHERE " + column + " = '" + datum + "';" //gets the ids from the db whcih match the column and datum
-	rows, err := dbValues.db.Query(sql_query)
+	sql_query := "SELECT id FROM " + documentName + " WHERE " + column + " = '" + datum + "';" //gets the ids from the db which match the column and datum
+	rows, err := db.Query(sql_query)
 	defer rows.Close()
 	if err != nil {
 		fmt.Println("Error getting data: ", err)
@@ -63,13 +40,15 @@ func getData(c *gin.Context) {
 		pgIDs = append(pgIDs, pgID) // generates a string of postgres IDs which match the criteria
 	}
 
-	columnNames := getColumnNames()                        // TODO; get this to return the column names in the correct order
-	fullJsonSlice := make([]map[string]string, len(pgIDs)) // creates a slice of maps as long as the length of PG IDs
-	for _, pgID := range pgIDs {                           // loops through the PG IDs
+	fullJsonSlice := make([]map[string]string, 0) // creates a slice of maps as long as the length of PG IDs
+	for _, pgID := range pgIDs {                  // loops through the PG IDs
+
 		jsonMap := make(map[string]string) //makes a map for each one
-		for i := 0; i < len(columnNames); i++ {
-			sql_query := "SELECT " + columnNames[i] + " FROM " + dbValues.pgTable + " WHERE id = " + pgID + ";" // queries the db for a column cells value matching the column name and the id
-			rows, err := dbValues.db.Query(sql_query)
+		for i := 0; i < len(columnHeaders); i++ {
+
+			sql_query := "SELECT " + columnHeaders[i] + " FROM " + documentName + " WHERE id = " + pgID + ";" // queries the db for a column cells value matching the column name and the id
+			rows, err := db.Query(sql_query)
+
 			defer rows.Close()
 			if err != nil {
 				fmt.Println("Error getting data: ", err)
@@ -77,21 +56,22 @@ func getData(c *gin.Context) {
 			for rows.Next() {
 				var value string
 				err := rows.Scan(&value) //retrieves the value and assigns it to a variable
+
 				if err != nil {
 					fmt.Println("Error scanning data: ", err)
 				}
 
-				jsonMap[columnNames[i]] = value //adds this key value pair to the map
+				jsonMap[columnHeaders[i]] = value //adds this key value pair to the map
 			}
 		}
-
 		fullJsonSlice = append(fullJsonSlice, jsonMap) //adds the map to the slice
 	}
 	c.JSON(200, fullJsonSlice) // makes it into json
 }
 
 func connectdb(pgUser string, pgPassword string) *sql.DB {
-
+	pgUser = "postgres"
+	pgPassword = "postgres"
 	connStr := "postgresql://localhost/importedExcelSheets?user=" + pgUser + "&password=" + pgPassword + "&sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
@@ -101,13 +81,10 @@ func connectdb(pgUser string, pgPassword string) *sql.DB {
 }
 func main() {
 
-	// dbValues := &ExcelData{pgUser, pgPassword, "importedExcelSheets", connectdb(pgUser, pgPassword)}
-	// fmt.Println("Enter your postgres username: ")
-	// fmt.Scanln(&pgUser)
-	// fmt.Println("Enter your postgres password: ")
-	// fmt.Scanln(&pgPassword)
 	// fmt.Println("Enter the name of the excel document: ")
 	// fmt.Scanln(&documentName)
+
+	documentName = "sampleDataShort" // this is the name of the excel document
 	started := time.Now()
 
 	f, err := excelize.OpenFile(documentName + ".xlsx")
@@ -123,10 +100,9 @@ func main() {
 
 	fmt.Printf("Total rows: %d\n", len(rows))
 
-	db := connectdb(pgUser, pgPassword)
+	db = connectdb(pgUser, pgPassword)
 
-	ColumnHeadersWithSpaces := rows[0]
-	var columnHeaders []string
+	ColumnHeadersWithSpaces := rows[0] // gets the column headers from the first row
 	for _, column := range ColumnHeadersWithSpaces {
 		column = strings.Replace(column, " ", "_", -1)
 		column = strings.Replace(column, "/", "_", -1)
@@ -137,7 +113,7 @@ func main() {
 		column = strings.Replace(column, ":", "", -1)
 		column = strings.Replace(column, "%", "", -1)
 		columnHeaders = append(columnHeaders, column)
-	}
+	} // replaces spaces with underscores and removes other characters which postgres doesn't like
 
 	_, err = db.Exec("CREATE TABLE " + documentName + " (id SERIAL PRIMARY KEY)")
 	if err != nil {
@@ -173,7 +149,7 @@ func main() {
 		id++
 	}
 
-	db.Close()
+	defer db.Close()
 	finished := time.Now()
 	fmt.Printf("Script took %s to run \n", finished.Sub(started))
 	router := gin.Default()
